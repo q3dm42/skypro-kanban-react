ad70544f9c15bddcf0b1772027b8d6d0a042ca44
q3dm42 <sergoteq@gmail.com>
2026-01-23 18:03:22 +0100

    Break App component into Header, PopNewCard, PopBrowse, PopUser components

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Column = ({ title, cards }) => {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">{cards}</div>
+    </div>
+  );
+};
+
+export default Column;
