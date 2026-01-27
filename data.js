55abf0afb31afc2324231423221e37eea7e22614
q3dm42 <sergoteq@gmail.com>
2026-01-27 10:15:05 +0100

    Add loading state and data structure for task management; refactor Header component for user modal. hw-02 Props done

---
+++ data.js
@@ -0,0 +1,45 @@
+const data = [
+  {
+    id: 1,
+    topic: "Web Design",
+    title: "Название задачи 1",
+    date: "30.10.23",
+    status: "Без статуса",
+    themeClass: "_orange"
+  },
+  {
+    id: 2,
+    topic: "Research",
+    title: "Название задачи 2",
+    date: "31.10.23",
+    status: "Нужен ревью",
+    themeClass: "_green"
+  },
+  {
+    id: 3,
+    topic: "Copywriting",
+    title: "Название задачи 3",
+    date: "01.11.23",
+    status: "В работе",
+    themeClass: "_purple"
+  },
+  {
+    id: 4,
+    topic: "Web Design",
+    title: "Название задачи 4",
+    date: "02.11.23",
+    status: "Тестирование",
+    themeClass: "_orange"
+  },
+  {
+    id: 5,
+    topic: "Research",
+    title: "Название задачи 5",
+    date: "03.11.23",
+    status: "Готово",
+    themeClass: "_green"
+  },
+
+];
+
+export default data;
