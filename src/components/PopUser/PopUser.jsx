bff65e57cb77ca76b582703fa44b3f5dac0395e7
q3dm42 <sergoteq@gmail.com>
2026-02-05 10:23:03 +0100

    style: convert PopUser (exit modal) component to styled-components

--- src/components/PopUser/PopUser.jsx
+++ src/components/PopUser/PopUser.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import {
+  PopExitWrapper,
+  PopExitContainer,
+  PopExitBlock,
+  PopExitTitle,
+  PopExitForm,
+  PopExitBtnYes,
+  PopExitBtnNo,
+} from "./PopUser.styled";
 
-const PopUser = () => {
+const PopUser = ({ isModalVisible }) => {
   return (
-    <div className="pop-exit" id="popExit">
-      <div className="pop-exit__container">
-        <div className="pop-exit__block">
-          <div className="pop-exit__ttl">
-            <h2>Выйти из аккаунта?</h2>
-          </div>
-          <form className="pop-exit__form" id="formExit" action="#">
-            <div className="pop-exit__form-group">
-              <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                <a href="modal/signin.html">Да, выйти</a>{" "}
-              </button>
-              <button className="pop-exit__exit-no _hover03" id="exitNo">
-                <a href="main.html">Нет, остаться</a>{" "}
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <PopExitWrapper id="popExit" className={isModalVisible ? "active" : ""}>
+      <PopExitContainer>
+        <PopExitBlock>
+          <PopExitTitle>Выйти из аккаунта?</PopExitTitle>
+          <PopExitForm id="formExit">
+            <PopExitBtnYes id="exitYes">
+              <a href="modal/signin.html">Да, выйти</a>
+            </PopExitBtnYes>
+            <PopExitBtnNo id="exitNo">
+              <a href="main.html">Нет, остаться</a>
+            </PopExitBtnNo>
+          </PopExitForm>
+        </PopExitBlock>
+      </PopExitContainer>
+    </PopExitWrapper>
   );
 };
 
