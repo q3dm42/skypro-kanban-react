9efed75aa1d48d074a5d80bdd4e7cf97b736c16a
q3dm42 <sergoteq@gmail.com>
2026-02-05 10:22:59 +0100

    style: convert Column component to styled-components

---
+++ src/components/Column/Column.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ColumnWrapper = styled.div`
+  width: 20%;
+  margin: 0 auto;
+  display: block;
+
+  @media (max-width: 1200px) {
+    width: 25%;
+  }
+
+  @media (max-width: 660px) {
+    width: 100%;
+  }
+
+  @media (max-width: 495px) {
+    width: 100%;
+  }
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 0 10px;
+  margin: 15px 0;
+
+  p {
+    color: #94A6BE;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+`;
+
+export const CardsWrapper = styled.div`
+  width: 100%;
+  display: block;
+  position: relative;
+`;
