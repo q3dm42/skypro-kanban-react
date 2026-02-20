f396d75df958c09c6529baf2aa5e677c0d366484
q3dm42 <sergoteq@gmail.com>
2026-02-20 12:04:18 +0100

    fix: remove Content-Type header from API requests
    - API rejects Content-Type: application/json header
    - Let browser set appropriate header automatically
    - This fixes registration and login API calls

--- src/services/api.js
+++ src/services/api.js
@@ -17,7 +17,6 @@ async function apiRequest(endpoint, options = {}) {
 	const token = localStorage.getItem("authToken");
 
 	const headers = {
-		"Content-Type": "application/json",
 		...options.headers,
 	};
 
